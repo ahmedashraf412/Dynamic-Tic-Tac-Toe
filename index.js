6555c8908c0037456a66b20458d91396e3e4ff65
AhmedAshraf412 <ahmed.oj.412@gmail.com>
2024-10-23 12:30:49 +0300

    ui build

--- index.js
+++ index.js
@@ -3,30 +3,98 @@ let rows = 0;
 let columns = 0;
 let winNumber = 0;
 let history = [];
+let gameBlocks = [];
+let currentPlayer = "X";
+let roundWon = false;
+let result = document.querySelector(".result");
+let newGame = document.querySelector(".restart");
+gameSubmit();
 
-getValue();
-
-
-
- function getValue() {
+function newGameFun(){
+newGame.addEventListener('click', ()=>{
+    resetGame();
+        document.querySelector(".current-player").innerHTML = `Current Player: ${currentPlayer}`;
+        document.querySelector(".result").innerHTML = `Result: `;
+       
+        if (rows >= 3 && columns >= 3 && winNumber >= 3) {
+            buildGame(rows, columns);
+            startGame();
+            newGameFun();
+        }
+        startGame();
+});
+}
+function gameSubmit() {
     submitBtn.addEventListener("click", () => {
-        
-            rows = document.querySelector("#rows").value;
-            columns = document.querySelector("#columns").value;
-            winNumber = document.querySelector("#win-number").value;
-            if (rows && columns && winNumber >= 3) {
-                buildGame();
-                startGame();
+        resetGame();
+        document.querySelector(".current-player").innerHTML = `Current Player: ${currentPlayer}`;
+        document.querySelector(".result").innerHTML = `Result: `;
+        rows = document.querySelector("#rows").value;
+        columns = document.querySelector("#columns").value;
+        winNumber = document.querySelector("#win-number").value;
+        // document.querySelector("#rows").value = "";
+        // document.querySelector("#columns").value = "";
+        // document.querySelector("#win-number").value = "";
+        if (rows >= 3 && columns >= 3 && winNumber >= 3) {
+            buildGame(rows, columns);
+            startGame();
+            newGameFun();
         }
         else {
             alert("Minimum Number is 3");
-            
+
         }
+        
     });
 }
-function buildGame(){
-    
+function buildGame(rows, columns) {
+    document.getElementById("xo-game").style.gridTemplateColumns = `repeat(${columns},70px)`;
+    document.getElementById("xo-game").style.gridTemplateRows = `repeat(${rows},70px)`;
+    for (i = 0; i < (rows * columns); i++) {
+        let createBlock = document.createElement('div');
+        createBlock.className = 'xo-block'
+        createBlock.id = `${i}`;
+        document.querySelector('.xo-game').appendChild(createBlock);
+    }
+    gameBlocks.push(...document.querySelectorAll('.xo-block'));
 }
-function startGame(){
-    
+function startGame() {
+    gameBlocks.forEach((block, index) => {
+        block.addEventListener('click', () => {
+            if (roundWon === true) {
+                return
+            }
+            if (result.innerHTML == `Result: ${currentPlayer} Wins!` || result.innerHTML == `Result:  ${currentPlayer} Wins!`) {
+                return;
+            }
+            if (block.innerHTML.trim() === "") {
+                block.innerHTML = `${currentPlayer}`;
+                currentPlayer = currentPlayer == "X" ? currentPlayer = "O" : currentPlayer = "X";
+                document.querySelector(".current-player").innerHTML = `Current Player: ${currentPlayer}`;
+            }
+            let countX = gameBlocks.filter(block => block.innerHTML === 'X').length;
+            let countO = gameBlocks.filter(block => block.innerHTML === 'O').length;
+            if (countX + countO === (rows * columns)) {
+                result.innerHTML = "Result: Draw!";
+                return; // Stop further actions if game is over
+            }
+        }
+        )
+    })
+}
+
+function resetGame() {
+    if (document.querySelector(".xo-game").firstChild == null) {
+        return
+    }
+    while (document.querySelector(".xo-game").firstChild) {
+        document.querySelector(".xo-game").removeChild(document.querySelector(".xo-game").lastChild);
+    }
+    gameBlocks = [];
+    currentPlayer = "X";
+    result.innerHTML = "Result: ";
+}
+
+function win(){
+
 }
